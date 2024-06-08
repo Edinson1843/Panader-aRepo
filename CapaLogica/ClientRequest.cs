c65106020c20f2cffdbd3e8bbb0a65bbb70a26ae
Leonardo Cesías Díaz <ccert26@hotmail.com>
2024-06-08 18:43:56 -0500

    Added ClientRequest.cs

--- CapaLogica/ClientRequest.cs
+++ CapaLogica/ClientRequest.cs
@@ -27,5 +27,77 @@ namespace CapaLogica
                 return new DataTable();
             }
         }
+
+        public static bool AddClient(string id, string nombre, string apellido, string telefono)
+        {
+            try
+            {
+                using (SqlConnection connection = Connection.GetConnection())
+                {
+                    string query = "INSERT INTO Cliente (id, nombre, apellido, telefono) VALUES (@id, @nombre, @apellido, @telefono)";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@id", id);
+                    command.Parameters.AddWithValue("@nombre", nombre);
+                    command.Parameters.AddWithValue("@apellido", apellido);
+                    command.Parameters.AddWithValue("@telefono", telefono);
+
+                    connection.Open();
+                    int rowsAffected = command.ExecuteNonQuery();
+                    return rowsAffected > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al agregar el cliente: " + ex.Message);
+                return false;
+            }
+        }
+
+        public static bool UpdateClient(string id, string nuevoNombre, string nuevoApellido, string nuevoTelefono)
+        {
+            try
+            {
+                using (SqlConnection connection = Connection.GetConnection())
+                {
+                    string query = "UPDATE Cliente SET nombre = @nuevoNombre, apellido = @nuevoApellido, telefono = @nuevoTelefono WHERE id = @id";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@id", id);
+                    command.Parameters.AddWithValue("@nuevoNombre", nuevoNombre);
+                    command.Parameters.AddWithValue("@nuevoApellido", nuevoApellido);
+                    command.Parameters.AddWithValue("@nuevoTelefono", nuevoTelefono);
+
+                    connection.Open();
+                    int rowsAffected = command.ExecuteNonQuery();
+                    return rowsAffected > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al actualizar el cliente: " + ex.Message);
+                return false;
+            }
+        }
+
+        public static bool DeleteClient(string id)
+        {
+            try
+            {
+                using (SqlConnection connection = Connection.GetConnection())
+                {
+                    string query = "DELETE FROM Cliente WHERE id = @id";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@id", id);
+
+                    connection.Open();
+                    int rowsAffected = command.ExecuteNonQuery();
+                    return rowsAffected > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al eliminar el cliente: " + ex.Message);
+                return false;
+            }
+        }
     }
 }

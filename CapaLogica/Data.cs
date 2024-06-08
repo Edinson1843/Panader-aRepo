7295fc1490899baa9a1828428281b03f708d7268
Leonardo Cesías Díaz <ccert26@hotmail.com>
2024-06-08 17:43:31 -0500

    Updated CapaLogica

---
+++ CapaLogica/Data.cs
@@ -0,0 +1,20 @@
+﻿using CapaDatos;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogica
+{
+    class DB_Connection
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = Connection.GetConnection();
+            Connection.CloseConnection(connection);
+            Console.ReadLine();
+        }
+    }
+}

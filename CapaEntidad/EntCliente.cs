faab7888f42624bb4e67b467a5a92a7cb1133183
Leonardo Cesías Díaz <ccert26@hotmail.com>
2024-06-08 21:18:10 -0500

    Fixed Code

---
+++ CapaEntidad/EntCliente.cs
@@ -0,0 +1,10 @@
+﻿namespace CapaEntidad
+{
+    public class EntCliente
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Telefono { get; set; }
+    }
+}

f90b2beaacf68c8c2205016a7d5310014bcc56eb
takizawa30 <107008674+takizawa30@users.noreply.github.com>
2024-07-10 04:06:01 -0500

    boton

--- PanaderiaRepo/Inicio.Designer.cs
+++ PanaderiaRepo/Inicio.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(303, 237);
+            this.button1.Location = new System.Drawing.Point(302, 180);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(187, 118);
             this.button1.TabIndex = 0;
@@ -44,7 +44,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(607, 237);
+            this.button2.Location = new System.Drawing.Point(590, 180);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(187, 118);
             this.button2.TabIndex = 1;
@@ -54,7 +54,7 @@
             // 
             // Inicio
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 30F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(22F, 49F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1101, 643);
             this.Controls.Add(this.button2);

5b3183c80d2429ef776315ffffc6e6dcc83eb45d
Edinson1843 <148985715+Edinson1843@users.noreply.github.com>
2024-07-10 12:48:25 -0500

    Delete CapaEntidad/EntProducto.cs
    ·

--- CapaEntidad/EntProducto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CapaEntidad
-{
-    public class EntProducto
-    {
-        public string id_producto { get; set; }
-        public string nombrepro { get; set; }
-        public int cantidad { get; set; }
-    }
-}

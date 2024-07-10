64bd61f230d37d2db99f3e1267aafa0ca697456a
Edinson1843 <148985715+Edinson1843@users.noreply.github.com>
2024-07-10 12:47:34 -0500

    Update EntCliente.cs
    entidades

--- CapaEntidad/EntCliente.cs
+++ CapaEntidad/EntCliente.cs
@@ -1,10 +1,108 @@
 ﻿namespace CapaEntidad
 {
+   namespace CapaEntidad
+{
     public class EntCliente
     {
-        public string Id { get; set; }
+        public int ClienteID { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
+        public int Estado { get; set; }
         public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public int DNI { get; set; }
     }
+
+    public class EntContrato
+    {
+        public int ContratoID { get; set; }
+        public int Cantidad { get; set; }
+        public int Estado { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Descripcion { get; set; }
+    }
+
+    public class EntContratoCliente
+    {
+        public int ClienteID { get; set; }
+        public int ContratoID { get; set; }
+    }
+
+    public class EntCronogramaPago
+    {
+        public int CronogramaPagoID { get; set; }
+        public int ContratoID { get; set; }
+        public DateTime FechaPagos { get; set; }
+        public string MetodoPago { get; set; }
+    }
+
+    public class EntOrdenProduccion
+    {
+        public int OrdenProduccionID { get; set; }
+        public string NombreEmpleado { get; set; }
+        public int Cantidad { get; set; }
+        public string Producto { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime FechaSalida { get; set; }
+        public int Estado { get; set; }
+    }
+
+    public class EntPedido
+    {
+        public int PedidoID { get; set; }
+        public int Estado { get; set; }
+        public int OrdenProduccionID { get; set; }
+        public int Cantidad { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime FechaEntrega { get; set; }
+    }
+
+    public class EntProducto
+    {
+        public int ProductoID { get; set; }
+        public int Estado { get; set; }
+        public int OrdenProduccionID { get; set; }
+        public string Nombre { get; set; }
+        public int PrecioUnidad { get; set; }
+        public int Cantidad { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime Vencimiento { get; set; }
+        public decimal Precio { get; set; }
+    }
+
+    public class EntComprobantePago
+    {
+        public int ComprobantePagoID { get; set; }
+        public int CronogramaPagoID { get; set; }
+        public int Monto { get; set; }
+        public int PedidoID { get; set; }
+        public DateTime Fecha { get; set; }
+        public int Estado { get; set; }
+    }
+
+    public class EntBoletaVenta
+    {
+        public int BoletaVentaID { get; set; }
+        public int ComprobantePagoID { get; set; }
+        public int ClienteID { get; set; }
+        public int MontoTotal { get; set; }
+        public string Fecha { get; set; }
+    }
+
+    public class EntInsumos
+    {
+        public int InsumosID { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        public string Descripcion { get; set; }
+        public string Nombre { get; set; }
+        public int Precio { get; set; }
+        public int Estado { get; set; }
+    }
+
+    public class EntProductoInsumos
+    {
+        public int InsumosID { get; set; }
+        public int ProductoID { get; set; }
+    }
+
 }

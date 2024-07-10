7c30cc4d13736552c68f0586a7b3e45cb1b2ad28
Andre Floreano <victorandrefs14@gmail.com>
2024-07-10 04:08:36 -0500

    #

--- PanaderiaRepo/Inicio.Designer.cs
+++ PanaderiaRepo/Inicio.Designer.cs
@@ -30,13 +30,14 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(303, 237);
+            this.button1.Location = new System.Drawing.Point(134, 252);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(187, 118);
+            this.button1.Size = new System.Drawing.Size(296, 181);
             this.button1.TabIndex = 0;
             this.button1.Text = "Área de Producción";
             this.button1.UseVisualStyleBackColor = true;
@@ -44,19 +45,30 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(607, 237);
+            this.button2.Location = new System.Drawing.Point(649, 252);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(187, 118);
+            this.button2.Size = new System.Drawing.Size(290, 181);
             this.button2.TabIndex = 1;
             this.button2.Text = "Área de Ventas";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Freestyle Script", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(242, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(579, 141);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "La Casa del Pan";
+            // 
             // Inicio
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 30F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 38F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1101, 643);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("Franklin Gothic Medium", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -64,6 +76,7 @@
             this.Name = "Inicio";
             this.Text = "Inicio";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,5 +84,6 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
     }
 }
